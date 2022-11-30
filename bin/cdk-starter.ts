ee93144773b80885aa9b397a76ce487c81021210
shinoda.hg <shinoda.hg@om.asahi-kasei.co.jp>
2022-11-30 14:55:00 +0900

    Rename lambda-cw-fs stack

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -3,8 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 import {CdkStarterStack} from '../lib/cdk-starter-stack';
 
 const app = new cdk.App();
-new CdkStarterStack(app, 'my-cdk-stack', {
-  stackName: 'my-cdk-stack',
+new CdkStarterStack(app, 'lambda-cw-fs-stack', {
+  stackName: 'lambda-cw-fs-stack',
   env: {
     region: process.env.CDK_DEFAULT_REGION,
     account: process.env.CDK_DEFAULT_ACCOUNT,
